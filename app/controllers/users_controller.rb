a40c81b7b172f68c3e3d907258f89074e394ee95
増田陽子 <masudayouko@masudayoukonoMacBook-Air.local>
2020-06-18 11:02:47 +0900

    m

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,19 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   	#@books = Book.where(user_id: @user.id)
   	@books = @user.books
-  	
+  	@book = Book.new
   end
+
   def index
+
+  end
+
+
+  def create
+  	@book = Book.new(book_params)
+  	
+    @book.save
+    redirect_to user_path(params[:id]), notice: 'Book was successfully created.'
   end
 
 end
